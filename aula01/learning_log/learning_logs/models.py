8dad2925137c027bd1c7131a9eefa208889f2c2d
guto-farias <gutofariasdev@gmail.com>
2024-03-27 23:28:32 -0300

    create learning_logs
    -create learning logs (project and app)
    -gnerate table topic
    -create admin
    -add a topic
    -create class entry

---
+++ aula01/learning_log/learning_logs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Um assunto sobre o qual o user está aprendendo"""
+    text = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        """Devolve uma representação em string do modelo"""
+        return self.text
+    
+class Entry(models.model):
+    """Algo específico aprendido sobre um assunto"""
+    topic = models.ForeignKey
