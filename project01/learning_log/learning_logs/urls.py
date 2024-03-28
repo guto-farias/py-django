b3dfdf76e0346b9908fb2509f407886b7c44a4a7
guto-farias <gutofariasdev@gmail.com>
2024-03-28 20:28:26 -0300

    some adjustments

--- project01/learning_log/learning_logs/urls.py
+++ project01/learning_log/learning_logs/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'),
-    path('topic/<topic_id>/', views.topic, name='topic'),
+    path('topics/<topic_id>/', views.topic, name='topic'),
     
 ]
