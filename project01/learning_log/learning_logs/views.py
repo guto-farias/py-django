c25c8fd8b456795a9e7d9e39945752c46790b517
guto-farias <gutofariasdev@gmail.com>
2024-03-28 20:13:38 -0300

    create html files

--- project01/learning_log/learning_logs/views.py
+++ project01/learning_log/learning_logs/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Topic
 
-def index():
-    return
+def index(request):
+    """index learning_logs"""
+    return render(request, 'learning_logs/index.html') #o request busca a pagina templates por padrão
 
-# Create your views here.
+
+def topics(request):
+    """view pra topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics} #recebe um dicionario
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """mostra um único assunto e suas entries"""
+    
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added') 
+    context = {'topic' : topic, 'entries' : entries}
+    return render(request, 'learning_logs/topic.html', context)
