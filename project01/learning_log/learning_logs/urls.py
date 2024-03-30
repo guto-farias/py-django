764cfe5625c6bd22b44eb8911dcc38005666b6f8
guto-farias <gutofariasdev@gmail.com>
2024-03-29 21:19:36 -0300

    create new templates

--- project01/learning_log/learning_logs/urls.py
+++ project01/learning_log/learning_logs/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'),
     path('topics/<topic_id>/', views.topic, name='topic'),
+    path('new_topic', views.new_topic, name='new_topic'),
+    path('new_entry/<topic_id>/', views.new_entry, name='new_entry'),
     
 ]
--- project01/learning_log/learning_logs/views.py
+++ project01/learning_log/learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     """index learning_logs"""
@@ -20,3 +23,38 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added') 
     context = {'topic' : topic, 'entries' : entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """add um novo assunto"""
+    if request.method != 'POST':
+        #nenhum dado submetido cria formulario em branco
+        form = TopicForm()
+    else:
+        #dados de POST submetidos, processamento
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics')) #usa reverse para puxar a url sem 'saber' a url, ou seja pelo name dado em urls.py
+        
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """add nova entrada para o assunto especifico"""
+    topic = Topic.objects.get(id = topic_id)#foreign key
+    
+    if request.method != 'POST':
+        # se é diferente de post: nenhum dado submetido cria formulario em branco
+        form = EntryForm()
+    else:
+        #dados de POST submetidos, processamento
+        form = EntryForm(data = request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)#new entry vira um objeto com os dados do form, "esperando pra ser commitado"
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    
+    context = {'topic':topic, 'form':form}
+    return render(request, 'learning_logs/new_entry.html', context)
+            
