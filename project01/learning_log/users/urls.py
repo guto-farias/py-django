85f9855b59d86ea9f5c874679b31b6a225bc3da2
guto-farias <gutofariasdev@gmail.com>
2024-03-31 21:07:05 -0300

    register implementation

--- project01/learning_log/users/urls.py
+++ project01/learning_log/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), #auth views traz a lógica de autenticação pronta
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
 ]
--- project01/learning_log/users/views.py
+++ project01/learning_log/users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def logout_view(request):
+    """Faz logout de user"""
+    
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    """Faz o cadastro de um novo user"""
+    if request.method != 'POST':
+        # exibe form em branco
+        form = UserCreationForm()
+    else:
+        # processa form preenchido
+        form = UserCreationForm(data = request.POST)
+        
+        if form.is_valid():
+            new_user = form.save()
+        # faz login e redireciona para index
+            authenticated_user = authenticate(username = new_user.username, password = request.POST['password1']) #autentica user
+            login(request, authenticated_user) #loga user
+            return HttpResponseRedirect(reverse('index'))
+        
+    context = {'form' : form}
+    return render(request, 'users/register.html', context)
