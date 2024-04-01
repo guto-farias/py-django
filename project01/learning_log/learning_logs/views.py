f66d4a214fd0951783e91a44c07d9962b73ae107
guto-farias <gutofariasdev@gmail.com>
2024-03-31 22:24:34 -0300

    updated control access

--- project01/learning_log/learning_logs/views.py
+++ project01/learning_log/learning_logs/views.py
@@ -1,29 +1,37 @@
 from django.shortcuts import render
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required #decorators são formas simples de alterar comportamentos de funçoes sem alterar o código
 
 def index(request):
     """index learning_logs"""
     return render(request, 'learning_logs/index.html') #o request busca a pagina templates por padrão
 
 
+@login_required
 def topics(request):
     """view pra topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics} #recebe um dicionario
     return render(request, 'learning_logs/topics.html', context)
 
-
+@login_required
 def topic(request, topic_id):
     """mostra um único assunto e suas entries"""
     
     topic = Topic.objects.get(id = topic_id)
+    
+    #garante que assunto pertence a user atual
+    if topic.owner != request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by('-date_added') 
     context = {'topic' : topic, 'entries' : entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """add um novo assunto"""
     if request.method != 'POST':
@@ -33,15 +41,22 @@ def new_topic(request):
         #dados de POST submetidos, processamento
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit = False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('topics')) #usa reverse para puxar a url sem 'saber' a url, ou seja pelo name dado em urls.py
         
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """add nova entrada para o assunto especifico"""
     topic = Topic.objects.get(id = topic_id)#foreign key
+    
+    #garante que assunto pertence a user atual
+    if topic.owner != request.user:
+        raise Http404
     
     if request.method != 'POST':
         # se é diferente de post: nenhum dado submetido cria formulario em branco
@@ -58,15 +73,20 @@ def new_entry(request, topic_id):
     context = {'topic':topic, 'form':form}
     return render(request, 'learning_logs/new_entry.html', context)
             
-
+@login_required
 def edit_entry(request, entry_id):
     """edita anotação/entrada"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
     
+    #garante que assunto pertence a user atual
+    if topic.owner != request.user:
+        raise Http404
+    
     if request.method != 'POST':
         #requisição inicial; preenche form com entrada atual
         form = EntryForm(instance=entry)
+        
         
     else: 
         #dados de post submetidos; processa
