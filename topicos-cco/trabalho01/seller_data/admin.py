10002ebb519a0d765f426025f630c43a68206dba
guto-farias <gutofariasdev@gmail.com>
2024-04-04 21:21:10 -0300

    delete trabalho01

--- topicos-cco/trabalho01/seller_data/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from seller_data.models import Sell
-
-admin.site.register(Sell)
-
-# Register your models here.
--- topicos-cco/trabalho01/seller_data/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = 'index'), 
-    path('new_sell', views.new_sell, name = 'new_sell'),
-]
